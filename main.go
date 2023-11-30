9bc74eba7c0064567a7f62ff3115c106383a589c
NitishKumar06 <justnitish06@gmail.com>
2023-11-30 11:55:43 +0530

    add new func
    Signed-off-by: NitishKumar06 <justnitish06@gmail.com>

--- main.go
+++ main.go
@@ -12,29 +12,8 @@ const (
 )
 
 func main() {
-	//date := "2020-03-02T00:00:00.000Z"
-	//parsedDate, _ := time.Parse(layout, date)
-	//cart := types.Cart{
-	//	ID:     1,
-	//	UserID: 1,
-	//	Date:   parsedDate,
-	//	Products: []types.Product{
-	//		{
-	//			ProductID: 1,
-	//			Quantity:  4,
-	//		},
-	//		{
-	//			ProductID: 2,
-	//			Quantity:  1,
-	//		},
-	//	},
-	//}
-	//
-	//JsonData, _ := json.Marshal(cart)
-	//fmt.Printf(string(JsonData))
 	router := httprouter.New()
 	router.GET("/carts/", operations.GetAllCarts)
 	router.GET("/carts/:id", operations.GetCart)
-	router.GET("/carts", operations.LimitResult)
 	log.Fatal(http.ListenAndServe(":8081", router))
 }
--- operations/operations.go
+++ operations/operations.go
@@ -2,14 +2,12 @@ package operations
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"github.com/nitishfy/rest-api/types"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
-	"strconv"
 )
 
 const APIUrl = "https://fakestoreapi.com/carts/"
@@ -64,48 +62,6 @@ func GetCart(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		log.Fatalf("error unmarshalling the response body: %v", err)
 		return
 	}
-
-	response, err := json.Marshal(cart)
-	if err != nil {
-		log.Fatalf("error marshalling the cart struct to json: %v", err)
-		return
-	}
-
-	w.Header().Set("Content-Type", "Application/json")
-	w.Write(response)
-}
-
-func LimitResult(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	limit := r.FormValue("limit")
-	URL := fmt.Sprintf("%s?limit=%d", APIUrl, limit)
-
-	resp, err := http.Get(URL)
-	if err != nil {
-		log.Fatalf("error generating the request: %v", err)
-		return
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("error reading the response body: %v", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	var cart []types.Cart
-	if err = json.Unmarshal(body, &cart); err != nil {
-		log.Fatalf("error unmarshalling the response body: %v", err)
-		return
-	}
-
-	limitInt, _ := strconv.Atoi(limit)
-
-	if limitInt > len(cart) {
-		limitInt = len(cart)
-	}
-
-	// Truncate the cart to limit times
-	cart = cart[:limitInt]
 
 	response, err := json.Marshal(cart)
 	if err != nil {
